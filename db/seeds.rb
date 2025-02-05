237c34ba73e4c9c73cd0428413c114a1b59f69ff
Maria Eduarda Couto Grossi <maria.grossi1@estudante.ufla.br>
2025-02-05 10:12:27 -0300

    Imagens dos produtos na home

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Produto.create!(
+  [
+    {
+      nome: "Blusa Preta",
+      preco: 49.90,
+      imagem: "blusafempreta.jpeg"
+    },
+    {
+      nome: "Calça Jeans",
+      preco: 99.90,
+      imagem: "calca.jpg"
+    }
+  ]
+)
